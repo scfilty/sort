aae376ef1c72785645167bcc4d66b7bdf992964e
scfilty <{ipsp.yossy@gmail.com}>
2019-06-26 02:25:49 +0900

    quick_sort.c end

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // 素数
-#define N 19
+#define N 2999
 
 int A[N];
 
@@ -18,22 +18,21 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+
   int i, j, pivot;
   pivot = A[n/2];
   A[n/2] = A[0];
   A[0] = pivot;
-  printf()
-  printf("pivot=%d\n",pivot);
   for(i = j = 1; i < n; i++){
     if(A[i] <= pivot){
       swap(A+i, A+j);
       j++;
     }
   }
-  if (n == 1) return;
+  if (n <= 1) return;
 
-  quick_sort(A,n/2);
-  
+  quick_sort(A,j);
+  quick_sort(A+j,n-j);
 
 }
 
@@ -44,11 +43,9 @@ int main(){
   for(i=2;i<N;i++){
     A[i] = (long long int) A[i-1] * A[1] % N;
   }
-  for (i = 0;i < N;i++){
-    printf("%d\n",A[i]);
-  }
+
   quick_sort(A, N);
   for(i=0;i<N;i++){
-    printf("%dth element is %d\n", i, A[i]);
+    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
   }
 }
