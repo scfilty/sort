0f4d9e0e2ee3c6e4687344bac99b3e4d316d329e
scfilty <{ipsp.yossy@gmail.com}>
2019-06-28 00:14:47 +0900

    no message

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -18,6 +18,34 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int i, j1, j2, pivot;
+  if (n <= 1) return;
+  if (n == 2){
+    if (A[0] <= A[1]) return; 
+  }
+  pivot = A[n/2];
+  A[n/2] = A[0];
+  A[0] = pivot;
+
+  for(i = j1 = 1; i < n; i++){
+    if(A[i] <= pivot){
+      swap(A+i, A+j1);
+      j1++;
+    }
+  }
+
+  swap(A+0,A+j1-1);
+
+
+  for (i = j2 = 0; i < j1 ; i++){
+    if(A[i] < pivot){
+      swap(A+i, A+j2);
+      j2++;
+    }
+  }
+  quick_sort(A,j2);
+  quick_sort(A+j1,n-j1);
+
 }
 
 int main(){
@@ -31,7 +59,7 @@ int main(){
 // すべての要素が同じ場合でも計算が早く終わるか確認する
 
   quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  for (i = 0; i < N; i++){
+    printf("%d = %d\n",i, A[i]);
   }
 }
