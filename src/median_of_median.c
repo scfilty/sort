d7eba1c97edf0882ff59abcb615df758cd0bf770
scfilty <{ipsp.yossy@gmail.com}>
2019-07-02 02:22:05 +0900

    no message

--- src/median_of_median.c
+++ src/median_of_median.c
@@ -4,6 +4,7 @@
 #define N 2999
 
 int A[N];
+int Ar[N];
 
 // *p と *q の値を入れ替える関数
 void swap(int *p, int *q){
@@ -13,7 +14,7 @@ void swap(int *p, int *q){
   *q = tmp;
 }
 
-int quick_select(int A[], int n, int k){
+int quick_select0(int A[], int n, int k){
   int i, j, pivot;
 
 // 真ん中の要素をピボットとする
@@ -28,10 +29,54 @@ int quick_select(int A[], int n, int k){
   }
 
   if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+  else if(j < k+1) return quick_select0(A+j, n-j, k-j);
+  else return quick_select0(A+1, j-1, k);
 }
 
+
+int select_pivot(int A[],int n){
+  int B[n],i;
+
+  if (n <= 5) return quick_select0(A,n,n/2);
+  else
+  {
+    for (i = 0; i < n/5; i++){
+      B[i] = quick_select0(A+(5*i),5,2);
+    }
+    return select_pivot(B,n/5);
+  }
+}
+
+
+
+int quick_select(int A[],int n, int k){
+  int pivot,i,r1,r2;
+  if (n <= 5) return quick_select0(A,n,k);
+  
+  for (i = 0; i < n; i++){
+      Ar[i] = A[i];
+  }
+  pivot = select_pivot(Ar,n);
+  
+
+  for(i = r1 = 0; i < n; i++){
+    if(A[i] <= pivot){
+      swap(A+i, A+r1);
+      r1++;
+    }
+  }
+
+  for(i = r2 = 0; i < n; i++){
+    if(A[i] < pivot){
+      swap(A+i, A+r2);
+      r2++;
+    }
+  }
+
+  if (r2 < k && k <= r1) return A[k];
+  else if (r1 < k) return quick_select(A+r1,n-r1,k-r1);
+  else return quick_select(A,r2,k);  
+}
 
 int main(){
   int i;
