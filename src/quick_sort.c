b0a445fca44523c46f6bafd03a0bc051243e8608
scfilty <{ipsp.yossy@gmail.com}>
2019-07-29 01:12:53 +0900

    no message

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -18,11 +18,8 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
-
   int i, j, pivot;
-  pivot = A[n/2];
-  A[n/2] = A[0];
-  A[0] = pivot;
+  pivot = A[0];
   for(i = j = 1; i < n; i++){
     if(A[i] <= pivot){
       swap(A+i, A+j);
@@ -31,21 +28,19 @@ void quick_sort(int A[], int n){
   }
   if (n <= 1) return;
 
-  quick_sort(A,j);
+  quick_sort(A,j-1);
   quick_sort(A+j,n-j);
 
 }
 
 int main(){
   int i;
-  A[0] = 0;
-  A[1] = 17; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
+  A[0] = 1;
+  A[1] = 1;
+  A[2] = 1;
 
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+  quick_sort(A,3);
+  for (i = 0; i < 3; i++){
+    printf("%d ",A[i]);
   }
 }
